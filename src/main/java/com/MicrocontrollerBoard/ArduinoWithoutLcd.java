57f2fe59872b663bf754b34c4330630719af995f
Ackar <Ackar94@gmail.com>
2015-06-06 11:50:22 +0400

    added ArduinoWithoutLcd class + test to sketch2

---
+++ src/main/java/com/MicrocontrollerBoard/ArduinoWithoutLcd.java
@@ -0,0 +1,53 @@
+package com.MicrocontrollerBoard;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class ArduinoWithoutLcd implements BoardWithLcd {
+
+    private InputStream input;
+    private OutputStream output;
+
+    public ArduinoWithoutLcd(SerialPortFacade serialPort) throws IOException {
+        input = serialPort.getInput();
+        output = serialPort.getOutput();
+    }
+
+    public void setPin8On() throws IOException {
+        sendMessage("L1");
+    }
+
+    public void setPin9On() throws IOException {
+        sendMessage("L2");
+    }
+
+    public void setPin10On() throws IOException {
+        sendMessage("L3");
+    }
+
+    public void setPin11On() throws IOException {
+        sendMessage("L4");
+    }
+
+    public void setPin12On() throws IOException {
+        sendMessage("L5");
+    }
+
+    public void setPin13On() throws IOException {
+        sendMessage("L6");
+    }
+
+    public boolean isAvailable() {
+        /* ToDo: реализовать */
+        return true;
+    }
+
+    public void sendMessage(String message) throws IOException{
+        output.write(message.getBytes());
+    }
+
+    public void getConnection() {
+        /* ToDo: реализовать */
+    }
+}
---
+++ src/test/java/testSketch2.java
@@ -0,0 +1,35 @@
+import com.MicrocontrollerBoard.ArduinoWithoutLcd;
+import com.MicrocontrollerBoard.SerialPortFacade;
+
+public class testSketch2 {
+
+    public static void main(String[] args) {
+        SerialPortFacade port = null;
+        try {
+            port = new SerialPortFacade(9600, 1000, "COM4");
+            ArduinoWithoutLcd board = new ArduinoWithoutLcd(port);
+            board.setPin8On();
+            int currentPin = 8;
+            while(true) {
+                Thread.sleep(1000);
+                switch (currentPin) {
+                    case 8  : board.setPin8On();  break;
+                    case 9  : board.setPin9On();  break;
+                    case 10 : board.setPin10On(); break;
+                    case 11 : board.setPin11On(); break;
+                    case 12 : board.setPin12On(); break;
+                    case 13 : board.setPin13On();
+                    default: currentPin = 7; break;
+                }
+                currentPin++;
+            }
+        }
+        catch (Exception ignore) {/* NOP */}
+        finally {
+            if (port != null) {
+                port.close();
+            }
+        }
+    }
+
+}
